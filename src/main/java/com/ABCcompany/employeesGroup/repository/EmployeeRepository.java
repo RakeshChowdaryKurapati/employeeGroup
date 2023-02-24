750f63e780601cfaeb06a1b3fbf59eb37ccf5443
kurapati.naidu <kurapati.naidu@BA-000454139.TI.ads>
2023-02-24 15:33:12 +0530

    New Api

---
+++ src/main/java/com/ABCcompany/employeesGroup/repository/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.ABCcompany.employeesGroup.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ABCcompany.employeesGroup.model.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+	
+}
